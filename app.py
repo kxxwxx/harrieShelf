9cbb3e4323be5be4b9c616069defcb770791955c
kxxwxx <75294100+kxxwxx@users.noreply.github.com>
2021-01-19 21:23:20 +0900

    save me~~~

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def read_reviews():
 def read_articles():
     # 1. DB에서 리뷰 정보 모두 가져오기
     type = request.args.get('type')
-    reviews = list(db.reviews.find({'type': type}, {'_id': 0}))
+    reviews = list(db.reviews.find({'type': type}, {'_id': 0}).sort({'_id':-1}).limit(4))
     # 2. 성공 여부 & 리뷰 목록 반환하기
     return jsonify({'result': 'success', 'reviews': reviews})
 
